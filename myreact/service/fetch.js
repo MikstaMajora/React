83087baff44bb8f4ca0e3c2745c3fde546754149
Konstantin <banan03112000@gmail.com>
2022-10-23 19:04:34 +0600

    refactor

--- myreact/service/fetch.js
+++ myreact/service/fetch.js
@@ -1,9 +1,10 @@
+import axios from 'axios';
 
-export default{
-  API(q){
-    console.log("<<<<<<<"+q)
-    return fetch('http://api.openweathermap.org/data/2.5/weather?q='+q+',uk&APPID=72456375f61f475833df3addc204f85b')
-    .then((res)=>res.json())
-    .catch((err)=>console.log(err))
-  }
-}
+export const getCity = city => {
+  console.log('call');
+  return axios
+    .get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=72456375f61f475833df3addc204f85b`,
+    )
+    .catch(err => console.log(err));
+};
